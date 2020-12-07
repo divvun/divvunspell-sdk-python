f79085e7b13c4be490d994b7bdc7e2e55636a074
Eddie Antonio Santos <easantos@ualberta.ca>
2020-12-07 15:09:59 -0700

    change: port to pyo3

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,45 @@
 use std::sync::Arc;
 
-use cpython::{PyResult, py_class, py_module_initializer};
+use pyo3::prelude::*;
 use divvunspell::{archive, speller};
 
-py_module_initializer!(divvunspell, initdivvunspell, PyInit_divvunspell, |py, m| {
-    m.add_class::<SpellerArchive>(py)?;
-    m.add_class::<Speller>(py)?;
+#[pymodule]
+fn divvunspell(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<SpellerArchive>()?;
+    m.add_class::<Speller>()?;
+
     Ok(())
-});
+}
 
-py_class!(class SpellerArchive |py| {
-    data archive: Arc<dyn archive::SpellerArchive + Send + Sync>;
+#[pyclass]
+struct SpellerArchive {
+    archive: Arc<dyn archive::SpellerArchive + Send + Sync>,
+}
 
-    def __new__(_cls, path: String) -> PyResult<Self> {
+#[pymethods]
+impl SpellerArchive {
+    #[new]
+    fn new(path: String) -> PyResult<Self> {
         let ar = archive::open(std::path::Path::new(&path)).unwrap();
-        Self::create_instance(py, ar)
+        Ok(SpellerArchive { archive: ar })
     }
 
-    def speller(&self) -> PyResult<Speller> {
-        let speller = self.archive(py).speller();
-        Speller::create_instance(py, speller)
+    fn speller(&self) -> PyResult<Speller> {
+        let speller = self.archive.speller();
+        Ok(Speller { speller })
     }
-});
+}
 
-py_class!(class Speller |py| {
-    data speller: Arc<dyn speller::Speller + Send + Sync>;
+#[pyclass]
+struct Speller {
+    speller: Arc<dyn speller::Speller + Send + Sync>,
+}
 
-    def suggest(&self, word: String) -> PyResult<Vec<(String, f32)>> {
-        let speller = Arc::clone(&self.speller(py));
+#[pymethods]
+impl Speller {
+    fn suggest(&self, word: String) -> PyResult<Vec<(String, f32)>> {
+        let speller = Arc::clone(&self.speller);
         let results = speller.suggest(&word);
         Ok(results.into_iter().map(|x| (x.value.to_string(), x.weight)).collect::<Vec<_>>())
     }
-});
+}
